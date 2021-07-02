ed4b18d76552deac9d3533cd4e618af3bf5e77f2
dongfg <mail@dongfg.com>
2021-07-02 17:36:44 +0800

    debug: test daemon mode

--- issue.sh
+++ issue.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --rm -i -v "$(pwd)/out":/acme.sh -e Ali_Key=$Ali_Key -e Ali_Secret=$Ali_Secret neilpang/acme.sh --issue --dns dns_ali -d fun.dongfg.com -d *.fun.dongfg.com
+docker run --rm -id -v "$(pwd)/out":/acme.sh -e Ali_Key=$Ali_Key -e Ali_Secret=$Ali_Secret --net=host --name=acme.sh neilpang/acme.sh daemon
+docker exec acme.sh --server zerossl --register-account --eab-kid 4bxSadF_vlu61nSvnY2tpw --eab-hmac-key l8_R5ntb28gzo4xTAzxuOLfNbkXe5pGm_p9UvlnYqTVP6NDo4xBOb8xcXelflXcxvAl2NZLZ5bU-Vtfa2ABUlg
+docker exec acme.sh --server zerossl --issue --dns dns_ali -d fun.dongfg.com -d *.fun.dongfg.com
+docker stop acme.sh
