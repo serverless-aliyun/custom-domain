8d2c9f5a4865526a45bb0c16d940ad081ae70f21
dongfg <mail@dongfg.com>
2021-07-02 17:14:00 +0800

    debug: docker issue

--- issue.sh
+++ issue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v "$(pwd)/out":/acme.sh neilpang/acme.sh --issue --dns -d fun.dongfg.com -d *.fun.dongfg.com
+docker run --rm -i -v "$(pwd)/out":/acme.sh neilpang/acme.sh --issue --dns -d fun.dongfg.com -d *.fun.dongfg.com
