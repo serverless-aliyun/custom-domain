786646aeb000c3e21241246c3d408a7ac235b6a3
dongfg <mail@dongfg.com>
2021-07-02 17:19:58 +0800

    bugfix: add missing dns_ali

--- issue.sh
+++ issue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -i -v "$(pwd)/out":/acme.sh -e Ali_Key=$Ali_Key -e Ali_Secret=$Ali_Secret neilpang/acme.sh --issue --dns -d fun.dongfg.com -d *.fun.dongfg.com
+docker run --rm -i -v "$(pwd)/out":/acme.sh -e Ali_Key=$Ali_Key -e Ali_Secret=$Ali_Secret neilpang/acme.sh --issue --dns dns_ali -d fun.dongfg.com -d *.fun.dongfg.com
